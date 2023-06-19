372ac9ba8df1c03b768639b60567e426a8d4d077
Adrian Dlugosch <adridlugosch@gmail.com>
2023-06-20 01:15:33 +0200

    notify about new user joining the chat

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -8,5 +8,10 @@ namespace signalRdemo.Hubs
         {
             await Clients.All.SendAsync("OnMessageReceived", user, message);
         }
+
+        public async Task RegisterUser(string user)
+        {
+            await Clients.Others.SendAsync("OnNewUserRegistered", user);
+        }
     }
 }

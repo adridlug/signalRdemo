4382ffce346060dfed14a7fa2bcf6539bd14978d
Adrian Dlugosch <adridlugosch@gmail.com>
2023-06-20 00:52:49 +0200

    refactored chat.js

--- wwwroot/js/chat.js
+++ wwwroot/js/chat.js
@@ -1,41 +1,64 @@
-var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 
 var button = document.getElementById("sendButton");
 var msgList = document.getElementById("messagesList");
 
-button.addEventListener("click", function(event) {
-    var user = document.getElementById("userInput").value;
-    var msg = document.getElementById("messageInput").value;
+console.log("registering user...")
+var user = registerUserIfNeeded();
+var connection = setupAndConnectSignalR();
+registerUiEventHandlers();
 
-    connection.invoke("Broadcast", user, msg).catch(function (err) {
-        console.error(err.toString());
+
+function setupAndConnectSignalR() {
+    var _connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+    _connection.on("OnMessageReceived", function (user, message) {
+        var li = document.createElement("li");
+        li.textContent = user+ " wrote: "+message;
+        msgList.appendChild(li);
     });
-});
+    
+    _connection.onreconnecting(function (error) {
+        if (error)
+        {
+            console.log(error);
+        }
+        console.log("reconnecting");
+    });
+    
+    _connection.onreconnected( function (connectionId) {
+        console.log("Successfully reconnected");
+        if (connectionId)
+            console.log("ConnectionId: "+connectionId);
+    });
+    
+    _connection.start().then(function () { 
+        button.disabled = false;
+        console.log("connected");
+    });
 
+    return _connection;
+}
 
-button.disabled = true;
-
-connection.on("OnMessageReceived", function (user, message) {
-    var li = document.createElement("li");
-    li.textContent = user+ " wrote: "+message;
-    msgList.appendChild(li);
-});
-
-connection.onreconnecting(function (error) {
-    if (error)
+function registerUserIfNeeded() {
+    var usr = sessionStorage.getItem("user");
+    console.log("User: "+usr);
+    if (!usr)
     {
-        console.log(error);
-    }
-    console.log("reconnecting");
-});
+        console.log("calling prompt")
+        usr = prompt("Who are you?");
+        sessionStorage.setItem("user", usr);
+    } 
+    document.getElementById("labelUser").textContent = "Hi "+ usr
+    return usr;
+}
 
-connection.onreconnected( function (connectionId) {
-    console.log("Successfully reconnected");
-    if (connectionId)
-        console.log("ConnectionId: "+connectionId);
-});
-
-connection.start().then(function () { 
-    button.disabled = false;
-    console.log("connected");
-});
+function registerUiEventHandlers() {
+    button.addEventListener("click", function(event) {
+        //var user = document.getElementById("userInput").value;
+        var msg = document.getElementById("messageInput").value;
+    
+        connection.invoke("Broadcast", user, msg).catch(function (err) {
+            console.error(err.toString());
+        });
+    });
+}

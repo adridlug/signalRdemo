842c9ec30bc31e7c02808d02fef3f3d50b50531f
Adrian Dlugosch <adridlugosch@gmail.com>
2023-06-27 16:39:36 +0200

    improved ui

--- wwwroot/js/connection.js
+++ wwwroot/js/connection.js
@@ -1,41 +1,39 @@
 function setupAndConnectSignalR() {
-    var _connection = new signalR.HubConnectionBuilder()
+  var _connection = new signalR.HubConnectionBuilder()
     .withUrl("/chatHub")
     .build();
 
-    _connection.on("OnMessageReceived", function (senderUser, message) {
-        appendReceivedMessage(senderUser, message);
-    });
+  _connection.on("OnMessageReceived", function (senderUser, message) {
+    appendReceivedMessage(senderUser, message);
+  });
 
-    _connection.on("OnNewUserRegistered", function (user) {
-       appendUser(user);
-       ping(user);
-    });
+  _connection.on("OnNewUserRegistered", function (user) {
+    appendUser(user);
+    ping(user);
+  });
 
-    _connection.on("OnPingReceived", function(user) {
-        appendUser(user);
-    });
-    
-    _connection.onreconnecting(function (error) {
-        if (error)
-        {
-            console.log(error);
-        }
-        console.log("reconnecting");
-    });
-    
-    _connection.onreconnected( function (connectionId) {
-        console.log("Successfully reconnected");
-        if (connectionId)
-            console.log("ConnectionId: "+connectionId);
-    });
-    
-    _connection.start().then(function () { 
-        console.log("connected");
-        _connection.invoke("RegisterUser", senderUser).catch(function (err) {
-            console.error(err.toString());
-        })
-    });
+  _connection.on("OnPingReceived", function (user) {
+    appendUser(user);
+  });
 
-    return _connection;
+  _connection.onreconnecting(function (error) {
+    if (error) {
+      console.log(error);
+    }
+    console.log("reconnecting");
+  });
+
+  _connection.onreconnected(function (connectionId) {
+    console.log("Successfully reconnected");
+    if (connectionId) console.log("ConnectionId: " + connectionId);
+  });
+
+  _connection.start().then(function () {
+    console.log("connected");
+    _connection.invoke("RegisterUser", senderUser).catch(function (err) {
+      console.error(err.toString());
+    });
+  });
+
+  return _connection;
 }
